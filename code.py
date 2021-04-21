0dcb49ec2b54011ed7e58e9fcdb28d3dda96bc46
Yuvraj Jwala <74554809+yuvrajjwala@users.noreply.github.com>
2021-04-21 13:08:48 +0530

    Update and rename code to code.py

--- code.py
+++ code.py
@@ -1,2 +1,7 @@
 import pywhatkit
 pywhatkit.sendwhatmsg("+918789869147","Hello This will send whatsapp message automatically to the the no. at the given time using whatsapp web",10,16)
+
+
+
+
+#code by YUVRAJ JWALA
