4ca6bdf6e1c028796ea81034d05e471ff471017d
ki2ki2 <85406837+ki2ki2@users.noreply.github.com>
2021-10-04 01:36:09 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,5 @@
 import pywhatkit
-pywhatkit.sendwhatmsg("+918789869147","Hello This will send whatsapp message automatically to the the no. at the given time using whatsapp web",10,16)
+pywhatkit.sendwhatmsg("+918789869147 __phone number__","Hello This will send whatsapp message automatically to the the no. at the given time using whatsapp web",10,16)
 
 
 
