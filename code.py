637c8f99dcebd9b637739388260c3bb69aafb448
Snehasish Konger <87180847+Snehasish-Konger@users.noreply.github.com>
2021-10-04 18:07:07 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 import pywhatkit
 #one liner code in python to send whatsapp message
-pywhatkit.sendwhatmsg("+91********** __phone number__","Hello This will send whatsapp message automatically to the the no. at the given time using whatsapp web",10,16)
+pywhatkit.sendwhatmsg("this text should be rplaced by your phone number__phone number__","Hello This will send whatsapp message automatically to the the no. at the given time using whatsapp web",10,16)
 
 
 
